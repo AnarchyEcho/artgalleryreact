bd5e19201f0ea638f28384d47affbd8a35f5209a
anarchytrex <anarchytrex@hotmail.com>
2021-07-04 22:31:20 +0200

    v0.2.1

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -21,19 +21,19 @@ import Tiddy from "../media/tiddy.png"
 export default function Portfolio() {
   return (
     <div className="seo-pics">
-        <img src={Afk} alt="Immy's Fursona" width="350px" height="350px"/>
-        <img src={Arma} alt="Immy as an arma character with a duck to her left" width="350px" height="350px"/>        
-        <img src={Cute} alt="Immy self insert portrait" width="350px" height="350px"/>        
-        <img src={Cringe} alt="Animal Crossing special where Immy is holding an AC character" width="350px" height="350px"/>        
-        <img src={Long} alt="Album cover for Longcoats" width="350px" height="350px"/>        
-        <img src={Rain} alt="Immy and dog in the rain at a crossing" width="350px" height="350px"/>        
-        <img src={Water} alt="Immy laying on her back floating in the water" width="350px" height="350px"/>        
-        <img src={Aka} alt="Red japanese style assassin" width="350px" height="350px"/>                
-        <img src={Aoi} alt="Blue japanese style assassin" width="350px" height="350px"/>        
-        <img src={Dunkirk} alt="Man standing in front of a burning plane crash" width="350px" height="350px"/>
-        <img src={Shibe} alt="Immy and dog looking over the ocean with a cup of steaming tea" width="350px" height="350px"/>
-        <img src={Pippy} alt="Fat cat" width="350px" height="350px"/>
-        <img src={Tiddy} alt="Japanese style assassin with heterochromia of blue and green eyes" width="350px" height="350px"/>
+        <img src={Afk} width="350px" height="350px" alt="Immy's Fursona"/>
+        <img src={Arma} width="350px" height="350px" alt="Immy as an arma character with a duck to her left"/>        
+        <img src={Cute} width="350px" height="350px" alt="Immy self insert portrait"/>        
+        <img src={Cringe} width="350px" height="350px" alt="Animal Crossing special where Immy is holding an AC character"/>        
+        <img src={Long} width="350px" height="350px" alt="Album cover for Longcoats"/>        
+        <img src={Rain} width="350px" height="350px" alt="Immy and dog in the rain at a crossing"/>        
+        <img src={Water} width="350px" height="350px" alt="Immy laying on her back floating in the water"/>        
+        <img src={Aka} width="350px" height="350px" alt="Red japanese style assassin"/>                
+        <img src={Aoi} width="350px" height="350px" alt="Blue japanese style assassin"/>        
+        <img src={Dunkirk} width="350px" height="350px" alt="Man standing in front of a burning plane crash"/>
+        <img src={Shibe} width="350px" height="350px" alt="Immy and dog looking over the ocean with a cup of steaming tea"/>
+        <img src={Pippy} height="350px" width="350px" alt="Fat cat"/>
+        <img src={Tiddy} width="350px" height="350px" alt="Japanese style assassin with heterochromia of blue and green eyes"/>
     </div>
   )
 }
