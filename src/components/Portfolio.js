4ee8b6311ac5cca5c395cec73f6c3960df54d50d
anarchytrex <anarchytrex@hotmail.com>
2021-07-04 22:01:28 +0200

    v0.1.8

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -21,19 +21,19 @@ import Tiddy from "../media/tiddy.png"
 export default function Portfolio() {
   return (
     <div className="seo-pics">
-        <img src={Afk} alt="Seo bullshit" width="350px" height="350px"/>
-        <img src={Arma} alt="Seo bullshit" width="350px" height="350px"/>        
-        <img src={Cute} alt="Seo bullshit" width="350px" height="350px"/>        
-        <img src={Cringe} alt="Seo bullshit" width="350px" height="350px"/>        
-        <img src={Long} alt="Seo bullshit" width="350px" height="350px"/>        
-        <img src={Rain} alt="Seo bullshit" width="350px" height="350px"/>        
-        <img src={Water} alt="Seo bullshit" width="350px" height="350px"/>        
-        <img src={Aka} alt="Seo bullshit" width="350px" height="350px"/>                
-        <img src={Aoi} alt="Seo bullshit" width="350px" height="350px"/>        
-        <img src={Dunkirk} alt="Seo bullshit" width="350px" height="350px"/>
-        <img src={Shibe} alt="Seo bullshit" width="350px" height="350px"/>
-        <img src={Pippy} alt="Seo bullshit" width="350px" height="350px"/>
-        <img src={Tiddy}alt="Seo bullshit" width="350px" height="350px"/>
+        <img src={Afk} alt="Immy's Fursona" width="350px" height="350px"/>
+        <img src={Arma} alt="Immy as an arma character with a duck to her left" width="350px" height="350px"/>        
+        <img src={Cute} alt="Immy self insert portrait" width="350px" height="350px"/>        
+        <img src={Cringe} alt="Animal Crossing special where Immy is holding an AC character" width="350px" height="350px"/>        
+        <img src={Long} alt="Album cover for Longcoats" width="350px" height="350px"/>        
+        <img src={Rain} alt="Immy and dog in the rain at a crossing" width="350px" height="350px"/>        
+        <img src={Water} alt="Immy laying on her back floating in the water" width="350px" height="350px"/>        
+        <img src={Aka} alt="Red japanese style assassin" width="350px" height="350px"/>                
+        <img src={Aoi} alt="Blue japanese style assassin" width="350px" height="350px"/>        
+        <img src={Dunkirk} alt="Man standing in front of a burning plane crash" width="350px" height="350px"/>
+        <img src={Shibe} alt="Immy and dog looking over the ocean with a cup of steaming tea" width="350px" height="350px"/>
+        <img src={Pippy} alt="Fat cat" width="350px" height="350px"/>
+        <img src={Tiddy} alt="Japanese style assassin with heterochromia of blue and green eyes" width="350px" height="350px"/>
     </div>
   )
 }
