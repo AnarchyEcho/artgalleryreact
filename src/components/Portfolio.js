59481d4b99c138441f31a2c765facb1a2a188e8f
KodeAndre <andre.kodehode@gmail.com>
2021-07-12 09:11:03 +0200

    v0.3.4

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,21 +4,6 @@ import ReactDOM from "react-dom"
 // eslint-disable-next-line
 import Style from "../style.css"
 
-// import Afk from "../media/afk.jpg"
-// import Arma from "../media/arma.jpg"
-// import Cute from "../media/cute.jpg"
-// import Cringe from "../media/cringe.jpg"
-// import Long from "../media/long.jpg"
-// import Rain from "../media/rain.jpg"
-// import Water from "../media/water.jpg"
-// import Aka from "../media/aka.png"
-// import Aoi from "../media/aoi.png"
-// import Dunkirk from "../media/dunkirk.png"
-// import Shibe from "../media/shibe.png"
-// import Pippy from "../media/pippy.jpg"
-// import Tiddy from "../media/tiddy.png"
-
-
 export default function Portfolio() {
   return (
     <div className="seo-pics">
