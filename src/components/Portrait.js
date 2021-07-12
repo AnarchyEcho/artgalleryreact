6f51c6c9e043ab378cf412c14b9f1b6a5ec20a18
anarchytrex <anarchytrex@hotmail.com>
2021-07-12 19:58:08 +0200

    v0.3.4

--- src/components/Portrait.js
+++ src/components/Portrait.js
@@ -3,14 +3,12 @@ import React from "react"
 import ReactDOM from "react-dom"
 // eslint-disable-next-line
 import Style from "../style.css"
-// import portrait from ""
+import portrait from "../media/pinkgirl.png"
 
-function Portrait() {
+export default function Portrait() {
   return (
     <>
-      {/* <img src={portrait} alt="Portrait of author" width="500px"></img> */}
+      <img src={portrait} alt="Portrait of author" width="500px"></img>
     </>
   );
 }
-
-export default Portrait
