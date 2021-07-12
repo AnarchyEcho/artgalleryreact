2b0a034224d61ba229cd8d5395517bcc202e4558
KodeAndre <andre.kodehode@gmail.com>
2021-07-12 10:58:47 +0200

    oops

--- src/components/Portrait.js
+++ src/components/Portrait.js
@@ -3,12 +3,12 @@ import React from "react"
 import ReactDOM from "react-dom"
 // eslint-disable-next-line
 import Style from "../style.css"
-import portrait from "../media/cute.jpg"
+// import portrait from ""
 
 function Portrait() {
   return (
     <>
-      <img src={portrait} alt="Portrait of author" width="500px"></img>
+      {/* <img src={portrait} alt="Portrait of author" width="500px"></img> */}
     </>
   );
 }
