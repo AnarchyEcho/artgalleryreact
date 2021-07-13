3d64ee66beca80af2ad30439f5f95be2df08fc75
anarchytrex <anarchytrex@hotmail.com>
2021-07-13 23:13:06 +0200

    v0.3.7

--- src/components/Portrait.js
+++ src/components/Portrait.js
@@ -8,7 +8,9 @@ import portrait from "../media/pinkgirl.png"
 export default function Portrait() {
   return (
     <>
+      <a href="https://www.instagram.com/p/CNNl_5uh5iR/?utm_source=ig_web_copy_link%27%7D" target="_blank" rel="noreferrer">
       <img src={portrait} id="portraitimage" alt="Portrait of author" width="500px"></img>
+      </a>
     </>
   );
 }
