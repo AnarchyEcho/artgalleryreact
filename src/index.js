6f63fc71571fb558dd31480ee0272444502a1dc3
anarchytrex <anarchytrex@hotmail.com>
2021-07-13 23:29:43 +0200

    v0.3.8

--- src/index.js
+++ src/index.js
@@ -3,5 +3,9 @@ import ReactDOM from "react-dom"
 import App from "./App.js"
 // eslint-disable-next-line
 import Style from "./style.css"
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(<BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+</BrowserRouter>,
+document.getElementById("root"))
