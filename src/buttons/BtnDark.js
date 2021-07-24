7637795bdebd1d4e38f91d283cdb5587f98afbac
anarchytrex <anarchytrex@hotmail.com>
2021-07-24 23:03:53 +0200

    v0.5

--- src/buttons/BtnDark.js
+++ src/buttons/BtnDark.js
@@ -4,10 +4,14 @@ import React, { useState } from "react"
 import ReactDOM from "react-dom"
 // eslint-disable-next-line
 import Style from "../style.css"
+// eslint-disable-next-line
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// eslint-disable-next-line
 import { faAdjust as Adjust } from '@fortawesome/free-solid-svg-icons'
-
-
+// eslint-disable-next-line
+import Light from "../media/lightmode.png"
+// eslint-disable-next-line
+import Dark from "../media/darkmode.png"
 
 export default function BtnDark() {
     // target for darkmode css toggling
@@ -24,16 +28,22 @@ export default function BtnDark() {
       fontSize: "3em",
       cursor: "pointer",
       border: "none",
-      margin: "5px"
+      margin: "5px",
    }
+
+    const [Image, setImage] = React.useState(Light);
 
     // function that toggles targeted css from css file
     function darkmode() {
       element.classList.toggle("dark-mode");
+      
+      if (Image === Light) {setImage(Dark);}
+      if (Image === Dark) {setImage(Light);}
+
     }
     return (
     <div>
-      <button onClick={darkmode} style={style}><FontAwesomeIcon icon={Adjust} /></button>
+      <button onClick={darkmode} style={style}><img id="darkimg" src={Image} alt=""/></button>
     </div>
   );
 }
